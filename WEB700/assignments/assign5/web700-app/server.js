22b59f946b0ee96550185ff87874b31b526cabea
jdelacruz19-seneca <jdelacruz19@myseneca.ca>
2024-07-26 13:07:42 -0400

    edited courses module

--- WEB700/assignments/assign5/web700-app/server.js
+++ WEB700/assignments/assign5/web700-app/server.js
@@ -61,6 +61,14 @@ app.get("/students", (req, res) => {
     }
 });
 
+app.get("/courses", (req, res) => {
+    collegeData.getCourses().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json({ message: err });
+    });
+});
+
 app.get("/student/:num", (req, res) => {
     collegeData.getStudentByNum(req.params.num).then((data) => {
         res.json(data);
@@ -71,14 +79,6 @@ app.get("/student/:num", (req, res) => {
 
 app.get("/tas", (req, res) => {
     collegeData.getTAs().then((data) => {
-        res.json(data);
-    }).catch((err) => {
-        res.json({ message: err });
-    });
-});
-
-app.get("/courses", (req, res) => {
-    collegeData.getCourses().then((data) => {
         res.json(data);
     }).catch((err) => {
         res.json({ message: err });
